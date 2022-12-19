63a04b03bfcb3ea958f4891c591c5712f304c9e2
thuclan <lanbd1998@gmail.com>
2022-12-19 20:53:42 +0700

    set up initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import HomePage from "./page/home/HomePage";
+import { Route, Routes } from "react-router";
+import LogInPage from "./page/login/login";
+import Product from "./page/product/Product";
+import Layout from "./layout/Layout";
+import {
+  About,
+  Blog,
+  Contact,
+  CookiePolicy,
+  Forum,
+  PrivacyPolicy,
+  Support,
+  TermsCondition,
+} from "./components/page";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/forum" element={<Forum />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/cookie-policy" element={<CookiePolicy />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/support" element={<Support />} />
+        <Route path="/terms-conditions" element={<TermsCondition />} />
+        <Route path="/product-detail/:productCode" element={<Product />} />
+      </Route>
+      <Route path="/login" element={<LogInPage />} />
+    </Routes>
   );
 }
 
