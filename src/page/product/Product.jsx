92d56c1d5192602170d182c841600b5d1b0e5858
thuclan <lanbd1998@gmail.com>
2022-12-19 20:55:42 +0700

    add component page

---
+++ src/page/product/Product.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductItem from "../../components/product/ProductItem";
+
+function Product() {
+  return (
+    <>
+      <ProductItem />
+    </>
+  );
+}
+
+export default Product;
