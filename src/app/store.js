74ad4d557c4048fe1a9b92e3921f32ee349578bd
thuclan <lanbd1998@gmail.com>
2023-01-02 16:53:00 +0700

    store redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import filterReducer from '../features/course/filterSlice';
 
 export default configureStore({
 	reducer: {
-		counter: counterReducer
+		counter: counterReducer,
+		filter: filterReducer
 	}
 });
