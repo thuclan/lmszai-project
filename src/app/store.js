d6c8dad435155855f83471f518a9c816d6dad0dd
thuclan <lanbd1998@gmail.com>
2023-01-08 22:12:55 +0700

    update

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
-import filterReducer from '../features/course/filterSlice';
 
 export default configureStore({
 	reducer: {
